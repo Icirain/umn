3e2b41504c8196b9ad03c8c7a4c9c79a26efa779
Icirain <zuoxx067@umn.edu>
2016-11-07 10:54:59 -0600

    bbb

--- 2016_fall/CSCI 4041/programming_assignment1/partC.py
+++ 2016_fall/CSCI 4041/programming_assignment1/partC.py
@@ -8,6 +8,29 @@ class kmp_matching(object):
         file =  open(self.filename)
         self.W ='!'+file.readline().rstrip('\n')
         self.pattern ='!'+ file.readline().rstrip('\n')
-    
-test=kmp_matching('string_info.txt')
+        print(self.W,self.pattern)
+    def make_next(self,pattern):
+        next=list()
+        next.appen('!')
+        next.append(0)
+        k=0
+        for i in range(2,len(pattern)):
+            while k>0 and pattern[k+1]!=pattern[i]:
+                k=next[k]
+            if pattern[k+1] == pattern[i]:
+                k+=1
+            next.append(k)
+        return next
+    def kmp_matching(self):
+        next=make_next(self.pattern)
+        k=0
+        length=len(self.pattern)-1
+        for i in range(1,len(self.W)):
+            while k>0 and self.W[i]!=self.pattern[k+1]:
+                k=next[k]
+            if self.pattern[k+1]==self.pattern[i]:
+                k=k+1
+            if k==length:
+                print('START = '+(i-lenght+1))        
 
+test=kmp_matching('string_info.txt')
