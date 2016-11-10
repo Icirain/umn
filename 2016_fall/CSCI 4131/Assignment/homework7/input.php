d85c8b69eb3545953361a3c4db3734ea8bbeb745
Icirain <zuoxx067@umn.edu>
2016-11-10 13:21:38 -0600

    start for home work 7

---
+++ 2016_fall/CSCI 4131/Assignment/homework7/input.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<title>Form Input</title>
+  	<link rel="stylesheet" type="text/css" href="Style.css">
+  	<meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>Calendar Input</h1>
+    <p>Please provide a value for Event Name</p>
+    <p>Please select a value for Start Time</p>
+    <p>Please enter a value for Event and Time</p>
+    <p>Please enter a value for Event Location</p>
+    <form method='post' action='<<?php echo $_SERVER["PHP_SELF"]; ?>'>
+      
+    </form>
+  </body>
+</html>
